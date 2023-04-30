9eacf8265c37cdfbbecd9bf631db94b82a08cdc6
discphy <dinophy@nate.com>
2023-04-30 18:18:20 +0900

    feat(add): BindingResult of Controller

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -69,7 +69,7 @@ public class ValidationItemControllerV2 {
         // 검증에 실패하면 다시 입력 폼으로
         if (bindingResult.hasErrors()) {
             log.info("errors = {}", bindingResult);
-            return "validation/v1/addForm";
+            return "validation/v2/addForm";
         }
 
         // 성공 로직
