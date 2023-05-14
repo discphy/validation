e50fa4edb47358858520e24674a4f705e91b061c
discphy <dinophy@nate.com>
2023-05-15 08:26:08 +0900

    feat(add): Bean validation groups

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -10,17 +10,18 @@ import javax.validation.constraints.NotNull;
 @Data
 public class Item {
 
+    @NotNull(groups = UpdateCheck.class) // 수정 요구사항
     private Long id;
 
-    @NotBlank
+    @NotBlank(groups = { SaveCheck.class, UpdateCheck.class })
     private String itemName;
 
-    @NotNull
-    @Range(min = 1000, max = 1000000)
+    @NotNull(groups = { SaveCheck.class, UpdateCheck.class })
+    @Range(min = 1000, max = 1000000, groups = { SaveCheck.class, UpdateCheck.class })
     private Integer price;
 
-    @NotNull
-    @Max(9999)
+    @NotNull(groups = { SaveCheck.class, UpdateCheck.class })
+    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
