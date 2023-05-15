2c2f7fb70816e1947d17598797a2b5d80dba863f
discphy <dinophy@nate.com>
2023-05-16 08:28:28 +0900

    feat(add): Form object

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,27 +1,22 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
-import org.hibernate.validator.constraints.Range;
-
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Data
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class) // 수정 요구사항
+//    @NotNull(groups = UpdateCheck.class) // 수정 요구사항
     private Long id;
 
-    @NotBlank(groups = { SaveCheck.class, UpdateCheck.class })
+//    @NotBlank(groups = { SaveCheck.class, UpdateCheck.class })
     private String itemName;
 
-    @NotNull(groups = { SaveCheck.class, UpdateCheck.class })
-    @Range(min = 1000, max = 1000000, groups = { SaveCheck.class, UpdateCheck.class })
+//    @NotNull(groups = { SaveCheck.class, UpdateCheck.class })
+//    @Range(min = 1000, max = 1000000, groups = { SaveCheck.class, UpdateCheck.class })
     private Integer price;
 
-    @NotNull(groups = { SaveCheck.class, UpdateCheck.class })
-    @Max(value = 9999, groups = SaveCheck.class)
+//    @NotNull(groups = { SaveCheck.class, UpdateCheck.class })
+//    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
